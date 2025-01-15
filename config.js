0053d5691323bdcc4948761177953394f4b7c98d
piyush sunil gite <psgite96@gmail.com>
2025-01-15 15:37:25 +0530

    added

---
+++ config.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const isAdmin = process.env.IS_ADMIN === 'true';
+
+if (isAdmin) {
+  console.log("Admin privileges granted.");
+} else {
+  console.log("Access restricted. Admin only.");
+}
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const { resolve } = require('path');
+require('dotenv').config();
 
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
-
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.get('/', (_req, res) => {
+  if (process.env.IS_ADMIN === 'true') {
+    res.send({ message: "Welcome, jokerr!", data: ["smokerr a", "jokerr a"] });
+  } else {
+    res.send({ message: "Welcome, naruto!", data: ["datte", "bayo"] });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(3010, () => console.log(`Server is running on port http://localhost:${port}`));
